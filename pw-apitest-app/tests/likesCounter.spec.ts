8d4e8a091604ecdb30a9b1c89d5381d6d446b35d
test <martinsntest@gmail.com>
2025-08-13 16:54:19 -0600

    second commit

---
+++ pw-apitest-app/tests/likesCounter.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect, request} from '@playwright/test'
+
+test('Like counter increase', async ({page}) => {
+    await page.goto('https://conduit.bondaracademy.com/')
+    await page.getByText('Global Feed').click()
+    const firstLikeButton = await page.locator('app-article-preview').first().locator('button')
+
+    await firstLikeButton.click()
+})
+
